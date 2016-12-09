9ac47869cccd957df11ddc515ab118c4cd24e201
Shuxian <wu.shux@husky.neu.edu>
2016-12-09 00:17:25 -0500

    scala test

--- test/PredictorSpec.scala
+++ test/PredictorSpec.scala
@@ -1,9 +1,9 @@
 import models.Coordinate
 import org.scalatest.{FlatSpec, Matchers}
+import play.api.libs.json.JsValue
 import services.Predictor._
 
 import scala.util._
-import scala.math._
 
 /**
   * Created by Shuxian on 12/8/16.
@@ -20,11 +20,36 @@ class PredictorSpec extends FlatSpec with Matchers{
     val coord=getCoordinate(address)
 
 //    coord should matchPattern {
-//      case Success(Coordinate(_,_)) =>
+//      case Coordinate(_,_) =>
 //    }
     coord should be (Coordinate(40.7314123,-73.99698479999999))
 
   }
 
+  behavior of "getPopDensity"
 
+  it should "work with Coordinate(40.7314123,-73.99698479999999)" in {
+    val coord = Coordinate(40.7314123,-73.99698479999999)
+    val pop = getPopDensity(coord)
+//    pop should matchPattern {
+//      case Success(_) =>
+//    }
+    pop should be >=0.0
+  }
+
+  behavior of "getWeatherJson"
+
+  it should "work with Coordinate(40.7314123,-73.99698479999999)" in {
+    val coord = Coordinate(40.7314123,-73.99698479999999)
+    val weatherJson=getWeatherJson(coord)
+    val test=weatherJson.toString()
+    val pattern = """(icon)""".r
+//    println(test)
+    pattern.findAllIn(test).groupCount should be > 0
+
+//    weatherJson should matchPattern {
+//      case Success(pattern.findAllIn(test).length)=>
+//    }
+
+  }
 }
